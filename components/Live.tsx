881e1f587f8f9051aa5704cc5d36010777ea548f
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-02-04 19:50:33 +0300

    add live avater stack functionality

--- components/Live.tsx
+++ components/Live.tsx
@@ -126,7 +126,7 @@ const Live = () => {
       } else if (e.key === "Escape") {
         updateMyPresence({ message: "" });
         setCursorState({ mode: CursorMode.Hidden });
-      } else if (e.key === "Control") {
+      } else if (e.key === "e") {
         setCursorState({ mode: CursorMode.ReactionSelector });
       }
     };
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "liveblocks.io" }],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
