2cc568f7405703bc5d8af1822f0bedb4ce5b5f0b
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-02-10 19:17:40 +0300

    fix drawing issue

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import {
   handleCanvasObjectScaling,
   handleCanvasSelectionCreated,
   handleCanvaseMouseMove,
+  handlePathCreated,
   handleResize,
   initializeFabric,
   renderCanvas,
@@ -168,6 +169,13 @@ export default function Page() {
       handleCanvasObjectScaling({
         options,
         setElementAttributes,
+      });
+    });
+
+    canvas.on("path:created", (options: any) => {
+      handlePathCreated({
+        options,
+        syncShapeInStorage,
       });
     });
 
